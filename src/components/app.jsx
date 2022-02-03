60739d081cd5c6e109d0eb958e2bfd7984be9eb3
Abimelec Figueroa <abimefigueroa.os@gmail.com>
2022-02-03 14:38:53 -0400

    Kepper part 2

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from './header';
 import Note from './note';
 import Footer from './footer';
+import notes from '../notes'; 
 
 function App(){
     return (
         <div>
         <Header />
-        <Note />
+        {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
         <Footer />
         </div>
 
